d38dc9fcec9007f3660c589d42da1e04990542bc
tammenterho <terho.mikko@gmail.com>
2024-03-12 16:26:39 +0200

    working game

--- Assets/LogicScript.cs
+++ Assets/LogicScript.cs
@@ -1,18 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class LogicScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public int playerScore;
+    public Text scoreText;
+    public GameObject gameOverScreen;
+
+    [ContextMenu("Increase Score")]
+    public void addScore(int scoreToAdd)
     {
-        
+        playerScore = playerScore + scoreToAdd;
+        scoreText.text = playerScore.ToString();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void restartGame()
     {
-        
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+
+    }
+
+    public void gameOver() {
+    gameOverScreen.SetActive(true);
     }
 }
--- Assets/PersonScript.cs
+++ Assets/PersonScript.cs
@@ -7,20 +7,31 @@ public class PersonScript : MonoBehaviour
 
     public Rigidbody2D myRigidbody;
     public float flapStrength;
+    public LogicScript logic;
+    public bool personIsAlive = true;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        logic = GameObject.FindGameObjectWithTag("Logic").GetComponent<LogicScript>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) == true)
+        if (Input.GetKeyDown(KeyCode.Space) && personIsAlive)
         {
-        myRigidbody.velocity = Vector2.up * 10;
+        myRigidbody.velocity = Vector2.up * flapStrength;
 
         }
 
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        logic.gameOver();
+        personIsAlive = false;
+        
     }
 }
